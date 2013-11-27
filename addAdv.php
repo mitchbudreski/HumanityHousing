749bf696618241af384455ae2a6a900982deff2f
pasquatch <bpasqualotto@gmail.com>
2013-11-26 21:33:49 -0800

    added line breaks after all labels

--- addAdv.php
+++ addAdv.php
@@ -4,31 +4,31 @@
 
 <h2>Add Advocate</h2>
 
-<label for="org">Organization:</label> 
+<label for="org">Organization:</label><br>
 <input type="text" id="org" name="advOrg" class="" placeholder="butts"><br>
 
-<label for="add">Address:</label>
+<label for="add">Address:</label><br>
 <input type="text" id="add" name="advAdd" class="" placeholder="butts"><br>
 
-<label for="city">City:</label>
+<label for="city">City:</label><br>
 <input type="text" id="city" name="advCity" class="" placeholder="butts"><br>
 
-<label for="postcode">Postal Code:</label>
+<label for="postcode">Postal Code:</label><br>
 <input type="text" maxlength="6" id="postcode" name="advPostCode" class="" placeholder="butts"><br>
 
-<label for="phone">Phone:</label>
+<label for="phone">Phone:</label><br>
 <input type="text" id="phone" name="advPhone" class="" placeholder="butts"><br>
 
-<label for="email">Email:</label>
+<label for="email">Email:</label><br>
 <input type="text" id="email" name="advEmail" class="" placeholder="butts"><br>
 
-<label for="officeopen">Office Hours:</label>
+<label for="officeopen">Office Hours:</label><br>
 <input type="text" id="officeopen" name="advOpen" class="" placeholder="butts">
 
-<label for="officecose">to </label>
+<label for="officecose">to </label><br>
 <input type="text" id="officeclose" name="advClose" class="" placeholder="butts"><br>
 
-<label for="description">Description:</label>
+<label for="description">Description:</label><br>
 <textarea rows="10" cols="30" id="description" name="advDesc" class="" placeholder="butts"></textarea>
 
 </form>
--- addApp.php
+++ addApp.php
@@ -4,17 +4,17 @@
 
 <h2>Add Application</h2>
 
-<label for="org">Organization:</label>
+<label for="org">Organization:</label><br>
 <!-- Not sure how to do this drop-down for existing inputted advocacy groups -->
-<select id="" name="" class=""><br>
+<select id="" name="" class="">
 <option value="">?</option>
 </select><br>
 
 <!-- I don't get how to do this either -->
-<label for="">Upload Application:</label>
+<label for="">Upload Application:</label><br>
 <input type="file" id="" name="" class=""><br>
 
-<label for="description">Description:</label>
+<label for="description">Description:</label><br>
 <textarea rows="10" cols="30" id="description" name="" class="" placeholder="butts"></textarea>
 
 </form>
