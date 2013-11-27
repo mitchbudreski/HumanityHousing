81ed5852e4a50aac0a184482807f38d860168f9f
Your Name Here <mitch.budreski@gmail.com>
2013-11-26 20:38:33 -0800

    Choke on my dick till you die from dickchoke
    dickchoke-o-rama 2014

--- index.php
+++ index.php
@@ -12,17 +12,27 @@
 
 		<!-- Wrapper Start -->
 		<div class="content-wrapper">
+
 		
 		
 			<!-- An Expander *** Copy Paste this code *** -->
 			<article class="expand">
 				<header>
-					<h4>Article Title<span class="icon-arrow-down"></span></h4>
+					<h1>Welcome</h1>
 					
 				</header>
 				
-				<div class="article-body">
-					<p> Nulla facilisi. In vel sem. Morbi id urna in diam dignissim feugiat. Proin molestie tortor eu velit. Aliquam erat volutpat. Nullam ultrices, diam tempus vulputate egestas, eros pede varius leo, sed imperdiet lectus est ornare odio. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin consectetuer velit in dui. Phasellus wisi purus, interdum vitae, rutrum accumsan, viverra in, velit. Sed enim risus, congue non, tristique in, commodo eu, metus. Aenean tortor mi, imperdiet id, gravida eu, posuere eu, felis. Mauris sollicitudin, turpis in hendrerit sodales, lectus ipsum pellentesque ligula, sit amet scelerisque urna nibh ut arcu. Aliquam in lacus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla placerat aliquam wisi. Mauris viverra odio. Quisque fermentum pulvinar odio. Proin posuere est vitae ligula. Etiam euismod. Cras a eros.</p>
+		<div class="landing">
+			
+			<p>DicksdicksdicksdicksdicksDicksdicksdicksdicksdicksDicksdicksdicksdicksdicksDicksdicksdicks
+			dicksdicksDicksdicksdicksdicksdicksDicksdicksdicksdicksdicksDicksdicksdicksdicksdicksDicksdicksd
+			icksdicksdicksDicksdicksdicksdicksdicksDicksdicksdicksdicksdicksDicksdicksdicksdicksdicksDicksdicks
+			dicksdicksdicksDicksdicksdicksdicksdicksDicksdicksdicksdicksdicksDicksdicksdicksdicksdicks
+			DicksdicksdicksdicksdicksDicksdicksdicksdicksdicksDicksdicksdicksdicksdicksDicksdicksdicksdicksdicksDicks
+			dicksdicksdicksdicksDicksdicksdicksdicksdicksDicksdicksdicksdicksdicksDicksdicksdicksdicksdicks</p>
+			dicks*500
+
+		</div>
 				</div>
 			
 			</article>
@@ -35,5 +45,4 @@
 
 <!-- Footer -->
 <?php include_once('footer.php'); ?>
-<!-- APRIL DAMASO Checking in -->
-<!-- mitch saying hi
+
--- menu.php
+++ menu.php
@@ -1,11 +1,19 @@
 	<nav class="menu">
 		<!-- Menu List -->
-			<ul>
-				<li><a href="#" alt="">Home</a></li>
-				<li><a href="#" alt="">Industry</a></li>
-				<li><a href="#" alt="">Case Studies</a></li>
-
-			</ul>
+			<div class="navleft">
+				<ul>
+					<li><a href="#" alt="">Housing Application</a></li>
+					<li><a href="#" alt="">Advocates</a></li>
+					<li><a href="#" alt="">Hotels</a></li>
+				</ul>
+			</div>
+			<div class="navright">
+				<ul>
+					<li><a href="#" alt="">About</a></li>
+					<li><a href="#" alt="">Contact</a></li>
+				</ul>
+			</div>
+			
 	<!-- Menu list End -->
 	</nav>
 	<!-- Menu End -->
