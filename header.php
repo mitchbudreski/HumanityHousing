8bbf8d29baf16ed1803c09cfcf80d4a0b01cc570
Your Name Here <mitch.budreski@gmail.com>
2013-11-26 17:54:00 -0800

    first push
    brandon is a dildo

---
+++ header.php
@@ -0,0 +1,12 @@
+<!-- Header Start -->
+<section class="header">
+	<header>
+		<a href="#" class="toggle-nav" alt="Link: Menu show">=</a>
+
+	</header>
+</section>
+<!-- Header End -->
+
+<!-- Menu Start -->
+
+
---
+++ menu.php
@@ -0,0 +1,12 @@
+	<nav class="menu">
+		<!-- Menu List -->
+			<ul>
+				<li><a href="#" alt="">Home</a></li>
+				<li><a href="#" alt="">Industry</a></li>
+				<li><a href="#" alt="">Case Studies</a></li>
+
+			</ul>
+	<!-- Menu list End -->
+	</nav>
+	<!-- Menu End -->
+		
