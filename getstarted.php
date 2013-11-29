32be0c0309c3ade382575d47634041f664b5ce0a
Your Name Here <mitch.budreski@gmail.com>
2013-11-28 23:10:41 -0800

    merge the motherfucker
    fjfjsl;dkfj

---
+++ getstarted.php
@@ -0,0 +1,39 @@
+<?php include_once('head.php'); ?>
+	<!-- Header -->
+	<?php include_once('header.php'); ?>
+		<!-- Menu -->
+		<?php include_once('menu.php'); ?>
+		
+		
+		<!-- Hero Start -->
+		<section class="hero"></section>
+
+		<!-- Hero End -->
+
+		<!-- Wrapper Start -->
+		<div class="content-wrapper">
+
+		
+		
+			<!-- An Expander *** Copy Paste this code *** -->
+			<article class="expand">
+				<header>
+			
+					
+				</header>
+				
+		<div class="landing infographic">
+		</div>
+
+			
+			</article>
+			<!-- End Expander -->
+			
+			
+			
+		</div>
+		<!-- Wrapper End -->
+
+<!-- Footer -->
+<?php include_once('footer.php'); ?>
+
